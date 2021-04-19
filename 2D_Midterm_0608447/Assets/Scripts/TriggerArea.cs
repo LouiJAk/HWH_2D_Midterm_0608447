9a41604b53c24cf97d1fb47d12ce9d73359b7825
LouiJAk <71476978+LouiJAk@users.noreply.github.com>
2021-04-20 00:01:16 +0800

    障礙物機關設置

--- 2D_Midterm_0608447/Assets/Scripts/TriggerArea.cs
+++ 2D_Midterm_0608447/Assets/Scripts/TriggerArea.cs
@@ -7,7 +7,7 @@ public class TriggerArea : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "石頭")
+        if (collision.tag == "大木箱")
         {
             stones[0].SetActive(false);
         }
